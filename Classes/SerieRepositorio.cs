1c979262fb97429c433792587163441857569d84
Nelson "AGE" Gama <nelsongama@yahoo.com>
2021-09-15 14:12:11 -0300

    Criando repositorio DIO.series CRUD

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using DIO.series.Interfaces;
+
+namespace DIO.series
+
+{
+
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+
+        public void Atualiza(int id, Serie entidade)
+        {
+            listaSerie[id] = entidade;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            listaSerie.Add(entidade);
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie  retornaporId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
